d6a49dbdbb08330c381164a2e0cf42b3fcb740a2
MD OWAISH RAZA ANSARI <owaishansari492@gmail.com>
2026-05-31 23:23:08 +0530

    Added budget tracking and budget status

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import json
+from datetime import date
 
 try:
     with open("expense_tracker_d1.json", "r") as file:
         Data = json.load(file)
 except FileNotFoundError:
     Data=[]
-    
+
+try:
+    with open("expense_budget.json", "r") as file1:
+        Budget = json.load(file1)
+except FileNotFoundError:
+    Budget = {
+        "Budget": 0
+    }
+        
 print("Welcome🙏, would you like to Calculate your EXPENSES?💸..(y/n)")
 
 def save_data():
     with open("expense_tracker_d1.json", "w") as file:
         json.dump(Data, file, indent=4)
+
+def calculate_total_spending():
+    
+    total_spending = 0
+    
+    for expense in Data:
+        total_spending += expense["Amount"]
+
+    return total_spending
 
 Reply = input()
 
@@ -29,21 +47,25 @@ while True:
                 "3. ✍️ Edit expenses\n"
                 "4. 🔴 Delete expense\n"
                 "5. 💰 Total Spending And Number of Expenses\n"
-                "6. 🏃🏻Exit:\n"
+                "6. 🔎 Search Any Expense\n"
+                "7. 🎯 Set Monthly Budget\n"
+                "8. 📊 View Monthly Budget\n"
+                "9. 🏃🏻Exit:\n"
                 "Enter choices: "
     )
 
     if choice == "1":
         
-        expense_name = input("Enter your Expense Name: ")
-        expense_category = input("Enter you Expense Category: ")
-        
+        expense_name = input("Enter your Expense Name: ").title()
+        expense_category = input("Enter you Expense Category: ").title()
+    
         try:           
             expense_amount = float(input("ENTER THE AMOUNT: "))
             expense_data = {
                 "Name": expense_name,
                 "Amount": expense_amount,
                 "Category": expense_category,
+                "Date": str(date.today())
             }
             Data.append(expense_data)
             save_data()
@@ -62,6 +84,7 @@ while True:
             print(f"👤 Name     : {expense["Name"]}")
             print(f"💰 Amount   : ₹{expense["Amount"]}")
             print(f"🏷️ Category : {expense["Category"]}")
+            print(f"📆 Date     : {expense["Date"]}")
             
             print("-----------------------------")           
     elif choice == "3":
@@ -77,9 +100,9 @@ while True:
             
             expense_index = expense_number - 1
 
-            new_name = input("Enter new expense name: ")
+            new_name = input("Enter new expense name: ").title()
             new_amount = float(input("Enter new amount: "))
-            new_category = input("Enter new category: ")
+            new_category = input("Enter new category: ").title()
             
             Data[expense_index]["Name"] = new_name
             Data[expense_index]["Amount"] = new_amount
@@ -111,15 +134,68 @@ while True:
             print("❌ Expense number doesn't exist.")
             
     elif choice == "5":
-        total_spending = 0
         
-        for expense in Data:
-            total_spending += expense["Amount"]
+        total_spending  = calculate_total_spending()
             
         print(f"💰 Total Spending: ₹{total_spending}")
         print(f"🗓️ Number of Expenses: {len(Data)}")
         
-    elif choice == "6":
+    elif choice =="6":
+        search_expense = input("Enter Your Expense name: ").lower()
+        
+        found = False
+        
+        for expense in Data:
+            
+            if expense["Name"].lower() == search_expense:
+                
+                found = True
+                
+                print("\n📌 Expense Found\n")
+                print(f"👤 Name     : {expense["Name"]}")
+                print(f"💰 Amount   : ₹{expense["Amount"]}")
+                print(f"🏷️ Category : {expense["Category"]}")
+                print(f"📆 Date     : {expense["Date"]}")
+                
+                break
+        
+        if not found:
+            print("❌ Expense Not Found...")
+    
+    elif choice == "7":
+        print("Let's Set a Monthly Budget For you!!🎯")
+        try:
+            monthly_budget = float(input("Enter your Monthly budget: "))
+            
+            budget_data = {
+                "Budget": monthly_budget,
+            }
+            Budget = budget_data
+            
+            with open("expense_budget.json", "w") as file1:
+                json.dump(budget_data, file1, indent=4)
+            
+            print(f"Budget of ₹{monthly_budget} is now locked!!🔒")
+            
+        except ValueError:
+            print("Please enter a valid Budget!!🙂")
+            
+    elif choice == "8":
+        monthly_budget = Budget["Budget"]
+        
+        total_spending = calculate_total_spending()
+        
+        remaining_budget = monthly_budget - total_spending
+        
+        print(f"\n🎯 Monthly Budget : ₹{monthly_budget}")
+        print(f"💸 Total Spent      : ₹{total_spending}")
+        
+        if remaining_budget >=0: 
+            print(f"💰 Remaining Budget : ₹{remaining_budget}")
+        else:
+            print(f"⚠️ Budget Exceeded By ₹{-remaining_budget}")
+            
+    elif choice == "9":
 
         print("Goodbye....🤝🏻")
         break
